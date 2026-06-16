ca925de445f15a5f611c388ce39de0c963ced115
Luiz Felipe Correia <lfc291006@gmail.com>
2026-06-15 21:43:23 -0300

    Adição de mais jogos no mock

--- src/app/mock-jogos.ts
+++ src/app/mock-jogos.ts
@@ -16,6 +16,17 @@ export const jogos: JogoInterface[] = [
 
     },
     {
+        nome: "God of War",
+        desenvolvedor: "Santa Monica Studio",
+        favorito: false,
+        genero: {
+            id: "hack-slash",
+            value: "Hack and Slash",
+            jogos: []
+        },
+        imagem: "https://greenhillszone.com/wp-content/uploads/2012/08/932295_76312_front.jpg"
+    },
+    {
         nome: "God of War II",
         desenvolvedor: "Santa Monica Studio",
         favorito: false,
@@ -27,6 +38,17 @@ export const jogos: JogoInterface[] = [
         imagem: "https://i5.walmartimages.com/asr/3ac8e0b5-76d1-4995-9570-d431696221eb_1.7ac304ae3667075f6348a7a065044d54.jpeg"
     },
     {
+        nome: "Fighting for One Piece",
+        desenvolvedor: "Flat-Out",
+        favorito: false,
+        genero: {
+            id: "fighting",
+            value: "Fighting",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/16471-fighting-for-one-piece-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
         nome: "Shadow of the Colossus",
         desenvolvedor: "Team Ico",
         favorito: false,
@@ -36,6 +58,17 @@ export const jogos: JogoInterface[] = [
             jogos: []
         },
         imagem: "https://th.bing.com/th/id/R.72e20001a12a26a8463ee3f8b6a9b5f2?rik=PltA6%2bqOM26yQA&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Devil May Cry",
+        desenvolvedor: "Capcom",
+        favorito: false,
+        genero: {
+            id: "hack-slash",
+            value: "Hack and Slash",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.1d44a01baba8c2de69642b07f93b2fec?rik=7gQekbHgcABifg&pid=ImgRaw&r=0"
     },
     {
         nome: "Devil May Cry 3",
@@ -57,7 +90,7 @@ export const jogos: JogoInterface[] = [
             value: "Action-adventure",
             jogos: []
         },
-        imagem: "https://tse3.mm.bing.net/th/id/OIP.VBIWgRpkiBv2xmeRnPjj8AHaLu?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+        imagem: "https://images-na.ssl-images-amazon.com/images/I/51K7SY7WRRL._AC_.jpg"
     },
     {
         nome: "Tekken 5",
@@ -115,6 +148,17 @@ export const jogos: JogoInterface[] = [
         imagem: "https://bdjogos.com.br/capas/16648-crash-twinsanity-playstation-2-capa-1.jpg"
     },
     {
+        nome: "Resident Evil: Code: Veronica X",
+        desenvolvedor: "Capcom",
+        favorito: false,
+        genero: {
+            id: "survival-horror",
+            value: "Survival horror",
+            jogos: []
+        },
+        imagem: "https://www.lukiegames.com/assets/images/ps2_resident_evil_code_veronica_x-124010.jpg"
+    },
+    {
         nome: "Resident Evil 4",
         desenvolvedor: "Capcom",
         favorito: false,
@@ -124,6 +168,39 @@ export const jogos: JogoInterface[] = [
             jogos: []
         },
         imagem: "https://www.vgdb.com.br/gf/fotos/games/media_49769/resident-evil-4--49769.jpg"
+    },
+    {
+        nome: "Fatal Frame II: Crimson Butterfly",
+        desenvolvedor: "Tecmo",
+        favorito: false,
+        genero: {
+            id: "survival-horror",
+            value: "Survival horror",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/10322-fatal-frame-ii-crimson-butterfly-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Silent Hill 2: Restless Dreams",
+        desenvolvedor: "Team Silent",
+        favorito: false,
+        genero: {
+            id: "survival-horror",
+            value: "Survival horror",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/23102017033217-silent-hill-2-ps2-cover.jpg?v=20250823174444"
+    },
+    {
+        nome: "Silent Hill 3",
+        desenvolvedor: "Team Silent",
+        favorito: false,
+        genero: {
+            id: "survival-horror",
+            value: "Survival horror",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/05122017144111-silent-hill-3-playstation-2-cover.jpg?v=20250823174444"
     },
     {
         nome: "Burnout 3: Takedown",
@@ -156,7 +233,18 @@ export const jogos: JogoInterface[] = [
             value: "Racing",
             jogos: []
         },
-        imagem: "https://i5.walmartimages.com/seo/Restored-Midnight-Club-3-Dub-Edition-Remix-PlayStation-2-Refurbished_b1f5146d-a934-45fe-923a-62a35c1a0a22.419af456af756979de77c35ad238f19e.jpeg"
+        imagem: "https://bdjogos.com.br/capas/17034-midnight-club-3-dub-edition-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Cars: Race-O-Rama",
+        desenvolvedor: "Incinerator Studios",
+        favorito: false,
+        genero: {
+            id: "racing",
+            value: "Racing",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/19468-cars-race-o-rama-playstation-2-capa-1.jpg?v=20250823174444"
     },
     {
         nome: "Jak X: Combat Racing",
@@ -167,7 +255,7 @@ export const jogos: JogoInterface[] = [
             value: "Racing",
             jogos: []
         },
-        imagem: "https://www.lukiegames.com/assets/images/PS2/ps2_jak_x_combat_racing_p_fjgo0a.jpg"
+        imagem: "https://alchetron.com/cdn/jak-x-combat-racing-68decf45-5b0f-404a-8c97-a98f36309c9-resize-750.jpeg"
     },
     {
         nome: "Marvel: Ultimate Alliance",
@@ -190,6 +278,17 @@ export const jogos: JogoInterface[] = [
             jogos: []
         },
         imagem: "https://th.bing.com/th/id/R.15473bd957f32ec32baa0ae0a4f135b1?rik=DhZRbXr2vk6HBg&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "X-Men: Legends II - Rise of Apocalypse",
+        desenvolvedor: "Raven Software",
+        favorito: false,
+        genero: {
+            id: "rpg",
+            value: "RPG",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/11913-x-men-legends-ii-rise-of-apocalypse-playstation-2-capa-1.jpg?v=20250823174444"
     },
     {
         nome: "Kingdom Hearts II",
@@ -236,6 +335,50 @@ export const jogos: JogoInterface[] = [
         imagem: "https://th.bing.com/th/id/R.b0885c6e953090794f4865ee69a742b1?rik=vBKXMUhRF%2bTk1g&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0401%2f5846%2f2113%2fproducts%2fPS2-SuperBombaPatch2010.fw_1200x1200.png%3fv%3d1679437714&ehk=5lgzdrgRha%2bLaeCzFZAsVNHl6XEkAMQ%2fC9PgqK4vb5g%3d&risl=&pid=ImgRaw&r=0"
     },
     {
+        nome: "Barbie Horse Adventures: Riding Camp",
+        desenvolvedor: "Pixel Tales",
+        favorito: false,
+        genero: {
+            id: "sports",
+            value: "Sports",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/19369-barbie-horse-adventures-riding-camp-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Tony Hawk's Pro Skater 3",
+        desenvolvedor: "Neversoft",
+        favorito: false,
+        genero: {
+            id: "sports",
+            value: "Sports",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/5719-Tony-Hawks-Pro-Skater-3-Playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Downhill Domination",
+        desenvolvedor: "Incognito Entertainment",
+        favorito: false,
+        genero: {
+            id: "sports",
+            value: "Sports",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/16362-downhill-domination-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Surf's Up",
+        desenvolvedor: "Ubisoft Divertissements Inc.",
+        favorito: false,
+        genero: {
+            id: "sports",
+            value: "Sports",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/19029-surfs-up-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
         nome: "Ghost Rider",
         desenvolvedor: "Climax Group",
         favorito: false,
@@ -256,6 +399,49 @@ export const jogos: JogoInterface[] = [
             jogos: []
         },
         imagem: "https://th.bing.com/th/id/R.aa14f93822bebded327532b0cb4359df?rik=XaZAC2uAuWfkYA&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Star Wars: Battlefront II",
+        desenvolvedor: "Pandemic Studios",
+        favorito: false,
+        genero: {
+            id: "fps",
+            value: "First-person shooter",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/17037-star-wars-battlefront-ii-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Half-Life",
+        desenvolvedor: "Gearbox Software",
+        favorito: false,
+        genero: {
+            id: "fps",
+            value: "First-person shooter",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/13745-half-life-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "007: Nightfire",
+        desenvolvedor: "Eurocom, Savage Entertainment",
+        favorito: false,
+        genero: {
+            id: "fps",
+            value: "First-person shooter",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/11937-007-nightfire-playstation-2-capa-1.jpg?v=20250823174444"
+    },
+    {
+        nome: "Call of Duty: World at War",
+        desenvolvedor: "Rebellion Developments",
+        favorito: false,
+        genero: {
+            id: "fps",
+            value: "First-person shooter",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/19364-call-of-duty-world-at-war-final-fronts-playstation-2-capa-1.jpg?v=20250823174444"
     }
-
 ];
