cf4ec798983f03b060361e74ee668184e1e5b3c0
Luiz Felipe Correia <lfc291006@gmail.com>
2026-06-28 01:07:38 -0300

    Melhorias de design no cabeçalho e na barra de pesquisa

--- src/app/componentes/cabecalho/cabecalho.ts
+++ src/app/componentes/cabecalho/cabecalho.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BarraPesquisa } from "../barra-pesquisa/barra-pesquisa";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cabecalho',
-  imports: [BarraPesquisa],
+  imports: [BarraPesquisa, RouterLink],
   templateUrl: './cabecalho.html',
   styleUrl: './cabecalho.css',
 })
